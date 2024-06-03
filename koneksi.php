e6d732a43a11f03738a89e2b60380136a64bddc0
hasanarofid <hasanarofid@gmail.com>
2024-06-03 13:28:11 +0700

    images afterlife

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost", "root", "", "inventory");
+$koneksi = mysqli_connect("localhost", "root", "hasanitki", "db_persedian");
 
 if (mysqli_connect_errno()) {
 	echo "Koneksi gagal".mysqli_connect_error();
