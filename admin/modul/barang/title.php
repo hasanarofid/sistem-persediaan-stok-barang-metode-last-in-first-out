fef32978c71dd8045add7492b461822477a33c6f
hasanarofid <hasanarofid@gmail.com>
2024-06-27 22:24:00 +0700

    import

--- admin/modul/barang/title.php
+++ admin/modul/barang/title.php
@@ -101,8 +101,12 @@
       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
         Tambah data
       </button>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#importModal">
+        Import Excel
+      </button>
 
       <!-- Modal -->
+       
       <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
           <div class="modal-content">
@@ -177,6 +181,32 @@
                 <button type="submit" name="simpan" class="btn btn-primary">Save changes</button>
               </div>
             </form>
+          </div>
+        </div>
+      </div>
+
+      <!-- Modal for importing data -->
+      <div class="modal fade" id="importModal" tabindex="-1" role="dialog" aria-labelledby="importModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="importModalLabel">Import Excel</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form action="simpan.php" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                  <label for="excelFile">Select Excel File</label>
+                  <input type="file" class="form-control-file" id="excelFile" name="file" required>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="submit" name="import" class="btn btn-primary">Import</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
